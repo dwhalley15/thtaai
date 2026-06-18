f18c7363cc9055586fe97523010febb81aa042bf
David Whalley <one_winged@hotmail.com>
2026-06-18 08:23:33 +0100

    updated files with new model selection option

--- Models/AiGenerationOptions.cs
+++ Models/AiGenerationOptions.cs
@@ -2,6 +2,7 @@ public class AiGenerationOptions
 {
     public string BaseUrl { get; set; } = "";
     public string ApiKey { get; set; } = "";
+    public string Model { get; set; } = "mistral";
     public int TimeoutSeconds { get; set; } = 300;
     public string PixabayKey { get; set; } = "";
 }
